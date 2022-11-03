e99f7a0a47ae498c83b7ab247fccb44bd30c41e8
a.kazachkov <114148644+kazachkov1@users.noreply.github.com>
2022-11-03 19:41:52 +0300

    Create made_tusk_02.py

---
+++ made_tusk_02.py
@@ -0,0 +1,7 @@
+city = ["Moscow", "Sankt-Putinsburg", "Sevastopol"]
+ppl = [12635, 5380, 390]
+print(f"Население {city[1]} - {ppl[1]} тысяч человек")
+allppl = 0
+for i in ppl:
+  allppl = allppl + int(i)
+print(f"Итого размер населения - {allppl} тысяч человек")
