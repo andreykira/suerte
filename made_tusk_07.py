20594ac35a3ce478b5e3b92c18732c7c7bb8059c
a.kazachkov <114148644+kazachkov1@users.noreply.github.com>
2022-12-04 20:13:18 +0300

    Create made_tusk_07.py

---
+++ made_tusk_07.py
@@ -0,0 +1,26 @@
+titles = {
+    'Кроссовки тип 3 - Adidas': '100000110',
+    'Мячик тип 2 - Adidas': '100000146',
+    'Кепка тип 1 - Adidas': '100000149',
+    'Ремень тип 2 - Nike': '100000194',
+    'Футболка тип 1 - Adidas': '100000224',
+    'Шапка тип 5 - Puma': '100000280',
+}
+sales = {
+    '100000110': [{'quantity': 31, 'price': 1637}],
+    '100000146': [ {'quantity': 4, 'price': 45}, {'quantity': 10, 'price': 48}],
+    '100000149': [ {'quantity': 28, 'price': 279}, {'quantity': 32, 'price': 291}],
+    '100000194': [{'quantity': 8, 'price': 220}, {'quantity': 1, 'price': 170}],
+    '100000224': [{'quantity': 61, 'price': 438}, {'quantity': 23, 'price': 302},  {'quantity': 50, 'price': 412}],
+    '100000280': [{'quantity': 26, 'price': 175}, ]
+}
+for x in titles:
+  products = sales[titles[x]]
+  a = 0
+  b = 0
+  c = 0
+  while c < len(products):
+    a += products[c]['quantity'] * products[c]['price']
+    b += products[c]['quantity']
+    c += 1
+  print(f"{x} - {a} шт, стоимость {b} руб")
