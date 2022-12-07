f528433241f7ce0a51135264c96e9b177bead3bd
krilns <89994630+krilns@users.noreply.github.com>
2022-12-07 20:16:46 +0300

    Update задание_08.py

--- задание_08.py
+++ задание_08.py
@@ -1,10 +1,12 @@
-employees = {'Alice' : 100000,
- 'Bob' : 99817,
- 'Carol' : 122908,
- 'Frank' : 88123,
- 'Eve' : 93121}
-top_earners = []
-for key, val in employees.items():
-    if val >= 100000:
-        top_earners.append((key,val))
-print(top_earners)
+def spisok():
+    employees = {'Alice' : 100000,
+    'Bob' : 99817,
+    'Carol' : 122908,
+    'Frank' : 88123,
+    'Eve' : 93121}
+    top_earners = []
+    for key, val in employees.items():
+        if val >= 100000:
+            top_earners.append((key,val))
+    print(top_earners)
+spisok()
